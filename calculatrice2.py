ba5f97d3c9acb530be36e1dcdf37172f96a9b804
ilian-bonsens <ilian.bonsens@laplateforme.io>
2023-11-29 16:25:51 +0100

    calculatrice plus optimisée et commentée

--- calculatrice2.py
+++ calculatrice2.py
@@ -1,28 +1,28 @@
+#variable historique en dehors de la fonction pour qu'elle ne soit pas écrasée à chaque boucle
 historique = ""
 
 def calculatrice():
-    global historique
-    while True:
-        try:
-            operateur = input("Type d'opération (+, -, *, /, **) : ")
-            if operateur in ['+', '-', '/', '*', '**']:
-                break
-            else:
-                print("Erreur : entrez un caractère valide (+, -, *, /, **) : ")
-        except ValueError:
-            print()
-
-    while True:
+    #permet l'appel de la variable externe à la fonction 
+    global historique 
+    #boucle infinie qui permet de rentrer les données de calcul à nouveau même en cas d'erreur
+    while True: 
         try:
             num1 = float(input("Entrez un nombre valide : "))
             num2 = float(input("Entrez un deuxième nombre valide : "))
+            operateur = input("Type d'opération (+, -, *, /, **) : ")
+            #erreur de la division par 0
             if operateur == '/' and num2 == 0:
                 print("Erreur : division par zéro. Veuillez entrer un autre nombre.")
+            #erreur du mauvais caractère en opérateur
+            elif operateur not in ['+', '-', '/', '*', '**']:
+                print("Erreur : entrez un caractère valide (+, -, *, /, **) : ")
             else:
                 break
-        except ValueError: 
+        #erreur de caractère invalide dans le float qui ne prend que des nombres
+        except ValueError:
             print("Erreur : entrez un caractère valide (nombre).")
 
+   #dictionnaire qui donne l'opération à effectuer en fonction de l'opérateur 
     operations = {
         "+": num1 + num2,
         "-": num1 - num2,
@@ -30,13 +30,14 @@ def calculatrice():
         "/": num1 / num2,
         "**": num1 ** num2
     }
-
+    
     resultat = operations[operateur]
 
     historique += f"{num1} {operateur} {num2} = {resultat}\n"
 
     print(f"Résultat : {resultat}")
 
+    #boucle concernant l'historique, pour l'afficher ou non et l'effacer ou non
     while True:
         ask_historique = input("Voulez-vous afficher l'historique (y/n) ou l'effacer (d) ? ")
         if ask_historique.lower() == 'y':
@@ -51,6 +52,7 @@ def calculatrice():
         else:
             print("Erreur : pour afficher l'historique entrez Oui (y) ou non (n).")
 
+#boucle pour la continuité de la fonction, pour ne pas que l'histo soit écrasé à chaque redémarrage
 while True:
     calculatrice()
     while True:
